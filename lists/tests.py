531353f4a62238cd31f38b1a5b931c73915e7841
João Campos <jbacampos@gmail.com>
2016-04-21 17:39:49 -0300

    Functional test now checks we can input a to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -16,6 +16,3 @@ class HomePageTest(TestCase):
 		self.assertTrue(response.content.startswith(b'<html>'))
 		self.assertIn(b'<title>To-Do lists</title>', response.content)
 		self.assertTrue(response.content.endswith(b'</html>'))
-
-
-	
